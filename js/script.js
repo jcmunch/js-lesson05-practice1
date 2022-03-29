d54e4101065a1ed6b56aae0d84dba33c3921ad19
Jennifer Münch <98417960+jcmunch@users.noreply.github.com>
2022-03-29 09:32:32 -0400

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+var fans = document.querySelector(".fans");
+var fish = document.querySelector(".fish");
+var pets = document.querySelector(".pets");
+//console.log(fans);
+
+fans.addEventListener("mouseover", function () {
+  fans.innerText = "123K";
+});
+
+fish.addEventListener("mouseover", function () {
+  fish.innerText = "47K";
+});
+
+pets.addEventListener("mouseover", function () {
+  pets.innerText = "20K";
+});
